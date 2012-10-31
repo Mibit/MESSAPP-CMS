3a2377d4d77e0abd28508e1f3958ff45381a6106
Gertsn <Gertsn@Gertsn-PC.int.fh-kufstein.ac.at>
2012-10-31 11:38:51 +0100

    New Update Studiengang View

--- application/views/studiengang.php
+++ application/views/studiengang.php
@@ -6,11 +6,15 @@
 					<table>
                         <tr>
                             <td><label for="stgName">Studiengangsname: </label><?php echo form_input(array("name" => "stgName", "value" => $stg->stgName)); ?></td>
+                        </tr><tr>
                             <td><label for="stgArt">Bachelor </label><?php echo form_radio(array("name" => "stgArt", "value" => "B")); ?>
                             	<label for="stgArt"> Master </label><?php echo form_radio(array("name" => "stgArt", "value" => "M")); ?>
                             </td>
+                        </tr><tr>
                             <td><label for="highlights">Highlights: </label><?php echo form_input(array("name" => "highlights", "value" => $stg->highlights)); ?></td>
+                        </tr><tr>
                             <td><label for="titelbild">Titelbild: </label><?php echo form_upload(array("name" => "titelbild", "value" => $stg->titelbild)); ?></td>
+                        </tr><tr>
                             <td><label for="freigabe">Freigeben: </label><?php echo form_checkbox(array("name" => "freigabe", "value" => $stg->freigabe)); ?></td>
                         </tr>
 					</table>
