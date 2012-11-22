26c3e23533d6fb08797bddbd7a7c6cfc3f632f01
mikevinmike <michael.dabernig@gmail.com>
2012-11-22 17:13:51 +0100

    Update Studiengang Sidebar

--- application/views/studiengang_sidebar.php
+++ application/views/studiengang_sidebar.php
@@ -1,12 +1,12 @@
 <div>
 	<div id="sidebarDiskriminatorBox">
-			<a href="#" onClick="javascript:redirectWithSave('<?php echo $my_url."index"; ?>');" id="sidebarDiskriminator">
+			<a onClick="javascript:redirectWithSave('<?php echo $my_url."index"; ?>');" id="sidebarDiskriminator">
 				Studieng&auml;nge
 			</a>
 	</div>
 	<?php foreach($items as $item) : ?>
 		<div class="sidebarItemBox">
-			<a href="#" onClick="javascript:redirectWithSave('<?php echo $my_url."edit/".$item->stgID; ?>');" class="sidebarItem <?php if($activeElementID==$item->stgID) echo " active "; ?>">
+			<a onClick="javascript:redirectWithSave('<?php echo $my_url."edit/".$item->stgID; ?>');" class="sidebarItem <?php if($activeElementID==$item->stgID) echo " active "; ?>">
 				<?php echo $item->stgKBez; ?>
 			</a>
 		</div>
