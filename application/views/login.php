ee96648dd8312ba82fd60837057dae12e68c02ce
Gertsn <Gertsn@Gertsn-PC.int.fh-kufstein.ac.at>
2012-12-05 10:34:38 +0100

    login.css

--- application/views/login.php
+++ application/views/login.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html lang="de-de">
-	<head>
-		<meta charset="utf-8" />
-		<title>Mess-App Verwaltung</title>
-		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
-		<link rel="stylesheet" href="<?php echo $template_url; ?>css/sample.css" />
-		<script src="<?php echo $template_url; ?>js/jquery.js"></script>
-		<script src="<?php echo $template_url; ?>js/login.js"></script>
-	</head>
-	<body>
-	
-		<?php echo $systemMessages; ?>
-	
-		<?php echo form_open("{$my_url}/authentification", array("name" => "loginForm", "id" => "loginForm")); ?>
-			<p>
-		    	<label for="username">Username</label>
-		        <?php echo form_input(array("name" => "username", "id" => "username"), $username); ?>
-		    </p>
-		    <p>
-		    	<label for="password">Passwort</label>
-		        <input name="password" id="password" type="password" />
-		    </p>
-		    <input type="submit" value="Login" />
-		<?php echo form_close(); ?>    
-		   
-		<noscript>
-			Warning! JavaScript must be enabled for proper operation of the Administrator back-end.
-		</noscript>
-		
-	</body>
+<head>
+<meta charset="utf-8" />
+<title>Mess-App Verwaltung</title>
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<link rel="stylesheet" href="<?php echo $template_url; ?>css/login.css" />
+<script src="<?php echo $template_url; ?>js/jquery.js"></script>
+<script src="<?php echo $template_url; ?>js/login.js"></script>
+</head>
+<body>
+
+	<img class="fhLogo"
+		src="<?php echo $template_url; ?>images/fh_logo.png"
+		style="width: 25%; height: 25%" />
+	<?php echo $systemMessages; ?>
+	<div class="loginBg">
+	<br />
+			<?php echo form_open("{$my_url}/authentification", array("name" => "loginForm", "id" => "loginForm")); ?>
+			<label for="username">Username</label>
+			<?php echo form_input(array("name" => "username", "id" => "username"), $username); ?>
+			<br /> <label for="password">Passwort</label>&nbsp;&nbsp;&nbsp;<input name="password"
+				id="password" type="password" /> <br /> <input type="submit"
+				value="Login" />
+
+			<?php echo form_close(); ?>
+	</div>
+	<noscript>Warning! JavaScript must be enabled for proper operation of
+		the Administrator back-end.</noscript>
+
+</body>
 </html>
